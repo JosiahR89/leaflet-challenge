f86a3250fdd5c94adeb97db4e9b3eee4e87dc592
JosiahR89 <82723815+JosiahR89@users.noreply.github.com>
2021-08-30 14:54:14 -0500

    logic.js update

--- logic.js
+++ logic.js
@@ -1,58 +1,100 @@
 // API endpoint as queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-d3.json(queryUrl).then(function (data) {
-    createFeatures(data.features);
-})
+//Perform a GET request to the query URL/
+d3.json(link).then(function (data) {
+  console.log(data);
+  createFeatures(data.features);
+});
 
+// Creating earthquake layers
+// Color code based on magnitude
+function chooseColor(magnitude) {
+  switch (true) {
+    case magnitude > 2.5:
+      return "#CD5C5C";
+    case magnitude > 2:
+      return "#F08080";
+    case magnitude > 1.5:
+      return "#FA8072";
+    case magnitude > 1:
+      return "#E9967A";
+    case magnitude > 0.5:
+      return "#FFA07A";
+    default:
+      return "#E6E6FA";
+  }
+}
+function styles(feature){
+  return{
+    fillColor: chooseColor(feature.properties.mag), 
+    fillOpacity: 1,
+    weight: 0.1,
+    radius: feature.geometry.coordinates[2]
+
+  }
+}
 function createFeatures(earthquakeData) {
+  // Creating popups for the features 
+  function onEachFeature(feature, layer)  {
+    layer.bindPopup(`<h3>${feature.properties.title}</h3>`);
+  }
+  // Create GeoJSON layer with earthquakeData object
+  var earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function(feature,latlng){
+      return L.circleMarker(latlng)
+    },
+    style: styles,
+    onEachFeature: onEachFeature
+  });
 
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+  // Send our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+// Create base layer
+function createMap(earthquakes) {
+  var lightMap = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+    attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  });
+
+  // Create a baseMap object
+  var baseMap = {
+    'Map':lightMap
+  };
+
+  // Create an overlay object to hold our overlay
+  var overlayMap = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquake layers to display on load
+  var myMap = L.map('map', {
+    center: [39.82, -98.57],
+    zoom: 5,
+    layers: [lightMap, earthquakes],
+});
+
+//Create legend
+var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 0.5, 1, 1.5, 2, 2.5],
+        colors = ["#E6E6FA", "#FFA07A", "#E9967A", "#FA8072", "#F08080", "#CD5C5C"];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + colors[i] + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
 
-    var earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature
-    });
+    return div;
+};
+legend.addTo(myMap);
 
-    createImageBitmap(earthquakes);
-}
 
-function createMap(earthquakes) {
-
-    // Create the base layers.
-    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
-  
-    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    });
-  
-    // Create a baseMaps object.
-    var baseMaps = {
-      "Street Map": street,
-      "Topographic Map": topo
-    };
-  
-    // Create an overlay object to hold our overlay.
-    var overlayMaps = {
-      Earthquakes: earthquakes
-    };
-  
-    // Create our map, giving it the streetmap and earthquakes layers to display on load.
-    var myMap = L.map("map", {
-      center: [
-        37.09, -95.71
-      ],
-      zoom: 5,
-      layers: [street, earthquakes]
-    });
-  
-    // Create a layer control
-    // Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(myMap);
-  
-  }
+  // Create a layer control, pass in baseMaps and overlayMaps, and add the layer control to the map
+  L.control.layers(baseMap, overlayMap, {collapsed: false}).addTo(myMap);
+  };
